6361bb563f504b57f660610f8d8d616423a8a0f7
Swasti Pandey <swastipandey69@gmail.com>
2026-06-28 01:39:30 +0530

    Completed Play Store review service

--- app/services/playstore.py
+++ app/services/playstore.py
@@ -1,15 +1,21 @@
 import re
 
-from google_play_scraper import app
-from google_play_scraper import reviews
+from google_play_scraper import app, reviews, Sort
 
 
 def extract_app_id(url: str):
+    """
+    Extract the application ID from a Google Play Store URL.
+    Example:
+    https://play.google.com/store/apps/details?id=com.spotify.music
+    →
+    com.spotify.music
+    """
 
     match = re.search(r"id=([A-Za-z0-9._]+)", url)
 
     if not match:
-        raise ValueError("Invalid Google Play URL")
+        raise ValueError("Invalid Google Play Store URL")
 
     return match.group(1)
 
@@ -24,28 +30,31 @@ def fetch_app_details(url: str):
         country="us"
     )
 
-    result, _ = reviews(
+    review_result, _ = reviews(
         app_id,
         lang="en",
         country="us",
+        sort=Sort.NEWEST,
         count=100
     )
 
     review_list = []
 
-    for review in result:
+    for review in review_result:
 
         review_list.append(review["content"])
 
     return {
 
-        "app_name": app_info["title"],
+        "app_id": app_id,
+
+        "name": app_info["title"],
 
         "developer": app_info["developer"],
 
         "rating": app_info["score"],
 
-        "total_reviews": len(review_list),
+        "reviews_count": len(review_list),
 
         "reviews": review_list
 
